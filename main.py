797408b8550bd72792c229ed17c6d7f56e40d866
rpadliwinata <rpadliwinata@gmail.com>
2023-10-10 09:27:15 +0700

    update

--- main.py
+++ main.py
@@ -1,55 +1,104 @@
-import socket
-import os
+from fastapi import FastAPI, Depends, HTTPException, UploadFile
+from fastapi.responses import FileResponse, JSONResponse
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from pydantic import SecretStr
+from datetime import datetime, timedelta
+import random
+from pathlib import Path  # Import the pathlib module
+from enum import Enum
+import shutil
 
-# Define the host and port to listen on
-HOST = '127.0.0.1'  # Listen on all available network interfaces
-PORT = 1000     # Use an available port number
+app = FastAPI()
 
-# Specify the directory to share
-SHARED_DIRECTORY = './soal'  # Replace with the actual directory path
+# Define the directory where files will be stored as a Path object
+upload_dir = Path("uploads")
 
-# Create a socket and bind it to the host and port
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
+# Ensure the upload directory exists
+upload_dir.mkdir(parents=True, exist_ok=True)
 
-# Listen for incoming connections
-server_socket.listen(1)  # Allow only one client to connect at a time
+# Generate a random code at startup
+current_code = str(random.randint(1000, 9999))
 
-print(f"Server is listening on {HOST}:{PORT}")
+def generate_new_code():
+    global current_code
+    current_code = str(random.randint(1000, 9999))
 
-while True:
-    # Accept a client connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address}")
+# Schedule the code generation every 5 minutes
+scheduler = BackgroundScheduler()
+scheduler.add_job(
+    generate_new_code,
+    trigger=IntervalTrigger(minutes=5),
+    next_run_time=datetime.now() + timedelta(minutes=5),
+)
+scheduler.start()
 
+class Matkul(str, Enum):
+    webpro = "webpro"
+    ppl = "ppl"
+    pbo = "pbo"
+    sister = "sister"
+    sisop = "sisop"
+    std = "std"
+
+@app.get("/getcode/")
+async def get_current_code(password: SecretStr):
+    if password.get_secret_value() == "passwordaslab":
+        return {"code": current_code}
+
+@app.post("/getfile/")
+async def get_file(code: str, matkul: Matkul):
     try:
-        # Receive the requested filename from the client
-        filename = client_socket.recv(1024).decode('utf-8')
-
-        # Build the full path to the requested file
-        file_path = os.path.join(SHARED_DIRECTORY, filename)
-
-        # Check if the file exists
-        if os.path.isfile(file_path):
-            # Send the file size to the client
-            file_size = os.path.getsize(file_path)
-            client_socket.send(str(file_size).encode('utf-8'))
-
-            # Send the file contents in chunks
-            with open(file_path, 'rb') as file:
-                data = file.read(1024)
-                while data:
-                    client_socket.send(data)
-                    data = file.read(1024)
-
-            print(f"Sent '{filename}' to {client_address}")
+        # Check if the requested file exists in the upload directory
+        file_path = upload_dir / f'{matkul}.pdf'
+        if not file_path.exists():
+            return JSONResponse(content={"error": "File not found"}, status_code=404)
+        
+        # Check if the provided code matches the current code
+        if code == current_code:
+            return FileResponse(file_path, headers={"Content-Disposition": f"attachment; filename=2304.08485.pdf"})
         else:
-            # If the file doesn't exist, send an error message
-            client_socket.send(b'File not found')
+            return JSONResponse(content={"error": "Wrong code"}, status_code=400)
+        
+    except Exception as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"}, status_code=500)
+
+@app.post("/{matkul}", include_in_schema=False)
+async def get_file(code: str, matkul: str):
+    try:
+        # Check if the requested file exists in the upload directory
+        file_path = upload_dir / f'{matkul}.pdf'
+        if not file_path.exists():
+            return JSONResponse(content={"error": "File not found"}, status_code=404)
+        
+        # Check if the provided code matches the current code
+        if code == current_code:
+            return FileResponse(file_path, headers={"Content-Disposition": f"attachment; filename=2304.08485.pdf"})
+        else:
+            return JSONResponse(content={"error": "Wrong code"}, status_code=400)
+        
+    except Exception as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"}, status_code=500)
+
+@app.post("/upload_file/")
+async def upload_file(password: SecretStr, file: UploadFile, matkul: Matkul):
+    try:
+        # Check if the provided password is correct
+        if password.get_secret_value() != "passwordaslab":  # Replace with your actual password
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        # Define the file path to save the uploaded file
+        file_path = upload_dir / f'{matkul}.pdf'
+
+        # Check if a file with the same name already exists and replace it
+        if file_path.exists():
+            file_path.unlink()
+
+        # Save the uploaded file to the specified path
+        with file_path.open("wb") as dest_file:
+            shutil.copyfileobj(file.file, dest_file)
+
+        return {"message": "File uploaded successfully"}
 
     except Exception as e:
-        print(f"Error: {e}")
-
-    finally:
-        # Close the client socket
-        client_socket.close()
+        return HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
